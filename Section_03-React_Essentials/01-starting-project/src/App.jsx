0cbb4ab72653860e87e2cd047322839333d55391
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-06 13:09:24 +0530

    complete 39

--- Section_03-React_Essentials/01-starting-project/src/App.jsx
+++ Section_03-React_Essentials/01-starting-project/src/App.jsx
@@ -1,19 +1,27 @@
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+
+    const Header = () => {
+
+        return (
+            <header>
+                <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+                <h1>React Essentials</h1>
+                <p>
+                    Fundamental React concepts you will need for almost any app you are
+                    going to build!
+                </p>
+            </header>
+        )
+    }
+
+    return (
+        <div>
+            <Header/>
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
