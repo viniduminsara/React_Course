64da94e421b5acbc505f0be83a1a4b76aff2366a
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-06 13:42:34 +0530

    complete 43

--- Section_03-React_Essentials/01-starting-project/src/App.jsx
+++ Section_03-React_Essentials/01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImage from './assets/react-core-concepts.png'
+
 function App() {
 
     const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
@@ -11,7 +13,7 @@ function App() {
 
         return (
             <header>
-                <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+                <img src={reactImage} alt="Stylized atom"/>
                 <h1>React Essentials</h1>
                 <p>
                     {description} React concepts you will need for almost any app you are
