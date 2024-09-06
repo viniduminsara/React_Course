e4ee1d23852a5f95a7a0bfa8d524fb64c35d6cee
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-06 17:09:05 +0530

    complete 51

--- Section_03-React_Essentials/01-starting-project/src/App.jsx
+++ Section_03-React_Essentials/01-starting-project/src/App.jsx
@@ -5,6 +5,10 @@ import TabButton from "./components/TabButton";
 
 function App() {
 
+    const handleSelect = () => {
+        console.log('selected');
+    }
+
     return (
         <div>
             <Header/>
@@ -21,10 +25,10 @@ function App() {
                 <section id='examples'>
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton>Components</TabButton>
-                        <TabButton>JSX</TabButton>
-                        <TabButton>Props</TabButton>
-                        <TabButton>State</TabButton>
+                        <TabButton onSelect={handleSelect}>Components</TabButton>
+                        <TabButton onSelect={handleSelect}>JSX</TabButton>
+                        <TabButton onSelect={handleSelect}>Props</TabButton>
+                        <TabButton onSelect={handleSelect}>State</TabButton>
                     </menu>
                 </section>
             </main>
--- Section_03-React_Essentials/01-starting-project/src/components/TabButton.jsx
+++ Section_03-React_Essentials/01-starting-project/src/components/TabButton.jsx
@@ -1,8 +1,8 @@
-const TabButton = ({ children }) => {
+const TabButton = ({ children, onSelect }) => {
 
     return (
         <li>
-            <button>{children}</button>
+            <button onClick={onSelect}>{children}</button>
         </li>
     )
 }
