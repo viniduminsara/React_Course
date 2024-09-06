bb7ed5125d05beb164d7919dc1bca55a1327b797
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-06 16:50:33 +0530

    complete 49

--- Section_03-React_Essentials/01-starting-project/src/App.jsx
+++ Section_03-React_Essentials/01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import {CORE_CONCEPTS} from './data'
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
 
@@ -16,6 +17,15 @@ function App() {
                         <CoreConcept {...CORE_CONCEPTS[2]}/>
                         <CoreConcept {...CORE_CONCEPTS[3]}/>
                     </ul>
+                </section>
+                <section id='examples'>
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton>Components</TabButton>
+                        <TabButton>JSX</TabButton>
+                        <TabButton>Props</TabButton>
+                        <TabButton>State</TabButton>
+                    </menu>
                 </section>
             </main>
         </div>
---
+++ Section_03-React_Essentials/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+const TabButton = ({ children }) => {
+
+    return (
+        <li>
+            <button>{children}</button>
+        </li>
+    )
+}
+
+export default TabButton;
