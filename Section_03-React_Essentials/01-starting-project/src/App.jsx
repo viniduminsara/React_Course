cc1df256e04166b31acfde52787a0fae5dc0b114
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-07 15:45:06 +0530

    complete section 3

--- Section_03-React_Essentials/01-starting-project/src/App.jsx
+++ Section_03-React_Essentials/01-starting-project/src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton";
 import {useState} from "react";
-import coreConcept from "./components/CoreConcept/CoreConcept";
 
 function App() {
 
@@ -39,8 +38,8 @@ function App() {
                             <h3>{EXAMPLES[selectedTopic].title}</h3>
                             <p>{EXAMPLES[selectedTopic].description}</p>
                             <pre>
-                            <code>{EXAMPLES[selectedTopic].code}</code>
-                        </pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                            </pre>
                         </div>
                     )}
                 </section>
