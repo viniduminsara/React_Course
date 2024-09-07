81e825520f989882b5ba6038b098c45953d6f872
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-07 13:31:03 +0530

    complete 52

--- Section_03-React_Essentials/01-starting-project/src/App.jsx
+++ Section_03-React_Essentials/01-starting-project/src/App.jsx
@@ -5,8 +5,8 @@ import TabButton from "./components/TabButton";
 
 function App() {
 
-    const handleSelect = () => {
-        console.log('selected');
+    const handleSelect = (selectedBtn) => {
+        console.log(selectedBtn);
     }
 
     return (
@@ -25,10 +25,10 @@ function App() {
                 <section id='examples'>
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton onSelect={handleSelect}>Components</TabButton>
-                        <TabButton onSelect={handleSelect}>JSX</TabButton>
-                        <TabButton onSelect={handleSelect}>Props</TabButton>
-                        <TabButton onSelect={handleSelect}>State</TabButton>
+                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
                 </section>
             </main>
