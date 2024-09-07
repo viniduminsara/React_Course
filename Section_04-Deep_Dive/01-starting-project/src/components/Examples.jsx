257b16dc0399957b7d409a9a82bf662111be20ce
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-07 17:48:54 +0530

    complete 67

--- Section_04-Deep_Dive/01-starting-project/src/components/Examples.jsx
+++ Section_04-Deep_Dive/01-starting-project/src/components/Examples.jsx
@@ -2,6 +2,7 @@ import TabButton from "./TabButton";
 import {EXAMPLES} from "../data";
 import {useState} from "react";
 import Section from "./Section";
+import Tabs from "./Tabs";
 
 const Examples = () => {
 
@@ -27,33 +28,36 @@ const Examples = () => {
 
     return (
         <Section title='Examples' id="examples">
-            <menu>
-                <TabButton
-                    isSelected={selectedTopic === 'components'}
-                    onClick={() => handleSelect('components')}
-                >
-                    Components
-                </TabButton>
-                <TabButton
-                    isSelected={selectedTopic === 'jsx'}
-                    onClick={() => handleSelect('jsx')}
-                >
-                    JSX
-                </TabButton>
-                <TabButton
-                    isSelected={selectedTopic === 'props'}
-                    onClick={() => handleSelect('props')}
-                >
-                    Props
-                </TabButton>
-                <TabButton
-                    isSelected={selectedTopic === 'state'}
-                    onClick={() => handleSelect('state')}
-                >
-                    State
-                </TabButton>
-            </menu>
-            {tabContent}
+            <Tabs buttons={
+                <>
+                    <TabButton
+                        isSelected={selectedTopic === 'components'}
+                        onClick={() => handleSelect('components')}
+                    >
+                        Components
+                    </TabButton>
+                    <TabButton
+                        isSelected={selectedTopic === 'jsx'}
+                        onClick={() => handleSelect('jsx')}
+                    >
+                        JSX
+                    </TabButton>
+                    <TabButton
+                        isSelected={selectedTopic === 'props'}
+                        onClick={() => handleSelect('props')}
+                    >
+                        Props
+                    </TabButton>
+                    <TabButton
+                        isSelected={selectedTopic === 'state'}
+                        onClick={() => handleSelect('state')}
+                    >
+                        State
+                    </TabButton>
+                </>
+            }>
+                {tabContent}
+            </Tabs>
         </Section>
     )
 }
---
+++ Section_04-Deep_Dive/01-starting-project/src/components/Tabs.jsx
@@ -0,0 +1,11 @@
+const Tabs = ({ children, buttons }) => {
+
+    return (
+        <>
+            <menu>{buttons}</menu>
+            {children}
+        </>
+    )
+}
+
+export default Tabs;
