52d9fdda76828cd5c38174bcc8097bcce27eef9b
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-07 15:13:59 +0530

    complete 55

--- Section_03-React_Essentials/01-starting-project/src/App.jsx
+++ Section_03-React_Essentials/01-starting-project/src/App.jsx
@@ -1,12 +1,15 @@
-import {CORE_CONCEPTS} from './data'
+import {CORE_CONCEPTS, EXAMPLES} from './data'
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton";
+import {useState} from "react";
 
 function App() {
 
+    const [ selectedTopic, setSelectedTopic ] = useState('components');
+
     const handleSelect = (selectedBtn) => {
-        console.log(selectedBtn);
+        setSelectedTopic(selectedBtn);
     }
 
     return (
@@ -30,6 +33,13 @@ function App() {
                         <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                         <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
+                    <div id='tab-content'>
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>{EXAMPLES[selectedTopic].code}</code>
+                        </pre>
+                    </div>
                 </section>
             </main>
         </div>
