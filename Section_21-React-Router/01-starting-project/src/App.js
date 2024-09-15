c9f494299d11bbc912acbc4454cc929048573e44
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-15 19:41:02 +0530

    complete 337

--- Section_21-React-Router/01-starting-project/src/App.js
+++ Section_21-React-Router/01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/RootLayout";
@@ -6,19 +6,20 @@ import ErrorPage from "./pages/Error";
 import ProductDetail from "./pages/ProductDetail";
 
 const router = createBrowserRouter([
-  { path: '/',
-    element: <RootLayout/>,
-    children: [
-      { path: '/', element: <Home/> },
-      { path: '/products', element: <Products/> },
-      { path: '/products/:productId', element: <ProductDetail/> },
-    ],
-    errorElement: <ErrorPage/>
-  },
+    {
+        path: '/',
+        element: <RootLayout/>,
+        children: [
+            {index: true, element: <Home/>},
+            {path: '/products', element: <Products/>},
+            {path: '/products/:productId', element: <ProductDetail/>},
+        ],
+        errorElement: <ErrorPage/>
+    },
 ])
 
 function App() {
-  return <RouterProvider router={router}/>;
+    return <RouterProvider router={router}/>;
 }
 
 export default App;
