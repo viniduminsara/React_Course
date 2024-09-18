1ddb00cb92d2467a5742d8d7f32ed2c903d7cfac
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-19 03:49:43 +0530

    complete 418

---
+++ Section_24-NextJS/01-starting-project/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage(){
+
+    return (
+        <main>
+            <h1>About Us</h1>
+        </main>
+    )
+}
---
+++ Section_24-NextJS/01-starting-project/app/blog/[slug]/page.js
@@ -0,0 +1,9 @@
+export default function BlogPostPage({ params }) {
+
+    return (
+        <main>
+            <h1>Blog post</h1>
+            <p>{params.slug}</p>
+        </main>
+    )
+}
