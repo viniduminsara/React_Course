078f3fda42acd944b6b2f7fafecb81960bd4a4ca
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-19 05:25:30 +0530

    complete 430

--- Section_24-NextJS/02-foodies-starting-project/components/main-header/main-header.js
+++ Section_24-NextJS/02-foodies-starting-project/components/main-header/main-header.js
@@ -1,10 +1,14 @@
+'use client'
+
 import Link from "next/link";
 import logoImage from '@/assets/logo.png'
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "@/components/main-header/main-header-bg";
+import {usePathname} from "next/navigation";
 
 export default function MainHeader() {
+    const path = usePathname();
 
     return (
         <>
@@ -18,10 +22,14 @@ export default function MainHeader() {
                 <nav className={classes.nav}>
                     <ul>
                         <li>
-                            <Link href='/meals'>Browse Meals</Link>
+                            <Link href='/meals' className={path.startsWith('/meals') ? classes.active : undefined}>
+                                Browse Meals
+                            </Link>
                         </li>
                         <li>
-                            <Link href='/community'>Foodies Community</Link>
+                            <Link href='/community' className={path.startsWith('/community') ? classes.active : undefined}>
+                                Foodies Community
+                            </Link>
                         </li>
                     </ul>
                 </nav>
